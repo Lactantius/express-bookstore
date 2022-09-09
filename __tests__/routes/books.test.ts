5dace4ea89bd9483cc573e5bf28f9284e3ea3365
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-09 08:06:05 -0500

    GET /books passing

---
+++ __tests__/routes/books.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest";
+
+import app from "../../src/app";
+import db from "../../src/db";
+import Book from "../../src/models/book";
+
+describe("Book routes test", () => {
+  beforeEach(async () => {
+    await db.query("DELETE FROM books");
+
+    const book1 = await Book.create({
+      isbn: "978-etc",
+      amazon_url: "https://amazon.com/test_book",
+      author: "Lector Scriptor",
+      language: "English",
+      pages: 500,
+      publisher: "Penguin",
+      title: "Test Book",
+      year: 2015,
+    });
+  });
+
+  afterEach(async () => {
+    await db.end();
+  });
+
+  test("can get list of books", async () => {
+    const response = await request(app).get("/books");
+
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+// describe("GET /books", () => {
+//   test("can get list of books", async () => {
+//     const response = await request(app).get("/books");
+
+//     expect(response.statusCode).toBe(200);
+//   });
+// });
